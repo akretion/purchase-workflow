971ad8de6d3ce24c78a59548b2e7e26564f3cc24
Pierrick Brun <pierrick.brun@akretion.com>
2019-03-28 16:34:21 +0100

    [FIX] declare filter in this module

--- purchase_quick/models/product_product.py
+++ purchase_quick/models/product_product.py
@@ -11,6 +11,9 @@ from odoo import api, fields, models
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    # for searching purpose
+    variant_specific_seller_ids = fields.One2many(
+        'product.supplierinfo', 'product_id')
     po_line_ids = fields.One2many(
         comodel_name='purchase.order.line', inverse_name='product_id',
         help='Technical: used to compute quantities to purchase.',
@@ -33,21 +36,17 @@ class ProductProduct(models.Model):
     def search(self, args, offset=0, limit=None, order=None, count=False):
         purchase = self.env['purchase.order'].browse(
             self.env.context.get('parent_id'))
-        if self.env.context.get('in_current_purchase') and purchase:
+        if self.env.context.get('in_current_parent') and purchase:
             po_lines = self.env['purchase.order.line'].search(
                 [('order_id', '=', purchase.id)])
             args.append(
                 (('id', 'in', po_lines.mapped('product_id').ids)))
-        if self.env.context.get('use_only_supplied_product') and purchase:
+        if self.env.context.get('for_current_supplier') and purchase:
             seller = purchase.partner_id
             seller = seller.commercial_partner_id or seller
-            supplierinfos = self.env['product.supplierinfo'].search(
-                [('name', '=', seller.id)])
-            args += [
-                '|',
-                ('product_tmpl_id', 'in',
-                    [x.product_tmpl_id.id for x in supplierinfos]),
-                ('id', 'in',
-                    [x.product_id.id for x in supplierinfos])]
+            args += ['|',
+                     ('variant_specific_seller_ids.name', '=', seller.id),
+                     '&', ('seller_ids.name', '=', seller.id),
+                     ('product_variant_ids', '!=', False)]
         return super(ProductProduct, self).search(
             args, offset=offset, limit=limit, order=order, count=count)
