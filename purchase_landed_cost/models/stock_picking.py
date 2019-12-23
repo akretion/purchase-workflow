190b86ca510ceac952a47b635a1b06065df76d08
clementmbr <clement.mombereau@akretion.com.br>
2019-12-23 19:45:42 -0300

    Hide landed cost button when empty

--- purchase_landed_cost/models/stock_picking.py
+++ purchase_landed_cost/models/stock_picking.py
@@ -1,11 +1,13 @@
 # Copyright 2013 Joaquín Gutierrez
 # Copyright 2014-2016 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    cost_distribution_ok = fields.Boolean(related='purchase_id.cost_distribution_ok', readonly='False')
 
     @api.multi
     def action_open_landed_cost(self):
