586bd7295401121d26ebd2ae161b8b6ecfe656ea
clementmbr <clement.mombereau@akretion.com.br>
2019-12-23 19:45:42 -0300

    Hide landed cost button when empty

--- purchase_landed_cost/models/purchase_order.py
+++ purchase_landed_cost/models/purchase_order.py
@@ -2,11 +2,25 @@
 # Copyright 2014-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    is_picking_done = fields.Boolean(
+        compute='_compute_is_picking_done', default='False')
+
+    cost_distribution_ok = fields.Boolean('Linked to Landed Costs', default='False')
+
+    @api.multi
+    def _compute_is_picking_done(self):
+        for order in self:
+            for picking in order.mapped('picking_ids'):
+                if picking.state == 'done':
+                    order.is_picking_done = True
+                else:
+                    order.is_picking_done = False
 
     @api.multi
     def action_open_landed_cost(self):
@@ -15,3 +29,9 @@ class PurchaseOrder(models.Model):
         lines = line_obj.search([('purchase_id', '=', self.id)])
         if lines:
             return lines.get_action_purchase_cost_distribution()
+
+    @api.onchange('partner_id', 'company_id')
+    def onchange_partner_id(self):
+        res = super(Partner, self).onchange_partner_id()
+        self.cost_distribution_ok = self.commercial_partner_id.cost_distribution_ok
+        return res
