7f1565256aa8b9ab5745514444a3dc8d4c1f2bde
clementmbr <clement.mombereau@akretion.com.br>
2019-12-26 20:39:17 -0300

    Auto-create Cost distribution from PO and Picking if no existing one

--- purchase_landed_cost/models/stock_picking.py
+++ purchase_landed_cost/models/stock_picking.py
@@ -7,12 +7,29 @@ from odoo import api, models, fields
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    cost_distribution_ok = fields.Boolean(related='purchase_id.cost_distribution_ok', readonly='False')
+    cost_distribution_ok = fields.Boolean(
+        related='purchase_id.cost_distribution_ok', readonly='False')
 
     @api.multi
     def action_open_landed_cost(self):
         self.ensure_one()
+
         line_obj = self.env['purchase.cost.distribution.line']
         lines = line_obj.search([('picking_id', '=', self.id)])
+        action = lines.get_action_purchase_cost_distribution()
+
         if lines:
-            return lines.get_action_purchase_cost_distribution()
+            return action
+        else:
+            distribution = self.env['purchase.cost.distribution'].create({})
+            import_picking_wizard = self.with_context(active_id=distribution.id).env['picking.import.wizard'].create({
+                'supplier': self.partner_id.id,
+                'pickings': [(6, 0, [self.id])],
+            })
+
+            import_picking_wizard.action_import_picking()
+
+            lines = line_obj.search([('picking_id', '=', self.id)])
+            action = lines.get_action_purchase_cost_distribution()
+
+        return action
