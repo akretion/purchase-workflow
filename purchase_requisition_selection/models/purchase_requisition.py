d9e49dc22b7887718b5106cc93a3120aeb3879ce
clementmbr <clement.mombereau@akretion.com.br>
2020-10-10 00:00:21 -0300

    [REF] rename purchase_tender_management to purchase_requisition_selection

--- purchase_requisition_selection/models/purchase_requisition.py
+++ purchase_requisition_selection/models/purchase_requisition.py
@@ -7,14 +7,35 @@ from odoo import _, models, fields
 class PurchaseRequisition(models.Model):
     _inherit = "purchase.requisition"
 
+    def _detect_lowest_price_line(self):
+        """For each product, set `is_lowest_price_line` to True for the order_line with
+        the lowest price_unit"""
+        line_ids = self.env["purchase.order.line"].search(
+            [
+                ("order_id", "in", self.purchase_ids.ids),
+                ("state", "not in", ["done", "cancel"]),
+            ]
+        )
+        product_ids = line_ids.mapped("product_id")
+
+        for prod in product_ids:
+            prod_line_ids = line_ids.filtered(lambda l: l.product_id == prod)
+
+            lowest_price_line_id = prod_line_ids.sorted(key=lambda l: l.price_unit)[0]
+            lowest_price_line_id.is_lowest_price_line = True
+
+            other_line_ids = prod_line_ids - lowest_price_line_id
+            other_line_ids.write({"is_lowest_price_line": False})
+
     def action_select_bids(self):
         self.ensure_one()
+        self._detect_lowest_price_line()
 
         tree_view_id = self.env.ref(
-            "purchase_tender_management.purchase_order_line_tree"
+            "purchase_requisition_selection.purchase_order_line_bid_selection_view_tree"
         )
         search_view_id = self.env.ref(
-            "purchase_tender_management.purchase_order_line_search"
+            "purchase_requisition_selection.purchase_order_line_search"
         )
 
         return {
