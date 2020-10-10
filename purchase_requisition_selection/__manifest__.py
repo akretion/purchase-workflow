8fab6833697321e4fbcf0a23abf6979c9791d49e
clementmbr <clement.mombereau@akretion.com.br>
2020-10-10 00:04:39 -0300

    [UPD] purchase_requisition_selection readme

--- purchase_requisition_selection/__manifest__.py
+++ purchase_requisition_selection/__manifest__.py
@@ -2,16 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Purchase Tender Management',
-    'description': """
-        Help selecting the best order lines from your suppliers bids""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://akretion.com/',
-    'depends': [
-        "purchase_requisition"
-    ],
+    "name": "Purchase Requisition Selection",
+    "description": """Help selecting the best order lines from your suppliers bids""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://akretion.com/",
+    "depends": ["purchase_requisition"],
     "data": [
         "views/purchase_order_views.xml",
         "views/purchase_requisition_views.xml",
