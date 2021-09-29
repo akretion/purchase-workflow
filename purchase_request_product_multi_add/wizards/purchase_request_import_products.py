d3ba54178a5849f4bc374fec9c423584da9d33fa
Florian Mounier <paradoxxx.zero@gmail.com>
2021-09-29 11:40:20 +0200

    [IMP] purchase_request_product_multi_add: Implement multi edit for quantities, date and estimated cost

--- purchase_request_product_multi_add/wizards/purchase_request_import_products.py
+++ purchase_request_product_multi_add/wizards/purchase_request_import_products.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class PurchaseRequestImportProducts(models.TransientModel):
@@ -6,93 +6,45 @@ class PurchaseRequestImportProducts(models.TransientModel):
     _description = "Purchase Request Import Products"
 
     products = fields.Many2many(comodel_name="product.product")
-    items = fields.One2many(
-        comodel_name="purchase.request.import.products.items",
-        inverse_name="wizard_id",
-    )
-
-    request_id = fields.Many2one(
-        comodel_name="purchase.request",
-        string="Purchase Request",
-        readonly=True,
-    )
 
     def create_items(self):
-        for wizard in self:
-            for product in wizard.products:
-                self.env["purchase.request.import.products.items"].create(
-                    {"wizard_id": wizard.id, "product_id": product.id}
-                )
-        view = self.env.ref(
-            "purchase_request_product_multi_add.view_import_product_to_purchase_request2"
+        purchase_request = self.env["purchase.request"].browse(
+            self.env.context.get("active_id", False)
         )
+        purchase_request_lines = [
+            self.env["purchase.request.line"].create(
+                {
+                    "request_id": purchase_request.id,
+                    "name": product.name,
+                    "product_id": product.id,
+                    "product_uom_id": product.uom_id.id,
+                }
+            )
+            for wizard in self
+            for product in wizard.products
+        ]
+
         return {
+            "name": "Set Products Informations",
             "type": "ir.actions.act_window",
-            "res_model": self._name,
-            "view_type": "form",
-            "view_mode": "form",
-            "views": [(view.id, "form")],
+            "res_model": "purchase.request.line",
+            "view_mode": "tree",
+            "views": [
+                (
+                    self.env.ref(
+                        "purchase_request_product_multi_add.view_edit_new_purchase_request_lines"
+                    ).id,
+                    "tree",
+                )
+            ],
+            "domain": [
+                ("request_id", "=", purchase_request.id),
+                (
+                    "id",
+                    "in",
+                    [prl.id for prl in purchase_request_lines],
+                ),
+            ],
             "target": "new",
-            "res_id": self.id,
             "context": self.env.context,
         }
-
-    @api.model
-    def _get_line_values(self, purchase_request, item):
-        purchase_request_line = self.env["purchase.request.line"].new(
-            {
-                "request_id": purchase_request.id,
-                "name": item.product_id.name,
-                "product_id": item.product_id.id,
-                "product_qty": item.quantity,
-                "product_uom_id": item.product_id.uom_id.id,
-                "date_required": item.date_required,
-                "estimated_cost": item.estimated_cost,
-            }
-        )
-        line_values = purchase_request_line._convert_to_write(
-            purchase_request_line._cache
-        )
-        return line_values
-
-    def select_products(self):
-        pr_obj = self.env["purchase.request"]
-        for wizard in self:
-            purchase_request = pr_obj.browse(self.env.context.get("active_id", False))
-
-            if purchase_request:
-                for item in wizard.items:
-                    vals = self._get_line_values(purchase_request, item)
-                    if vals:
-                        self.env["purchase.request.line"].create(vals)
-
-        return {"type": "ir.actions.act_window_close"}
-
-
-class PurchaseRequestImportProductsItem(models.TransientModel):
-    _name = "purchase.request.import.products.items"
-    _description = "PurchaseRequest Import Products Items"
-
-    wizard_id = fields.Many2one(
-        string="Wizard", comodel_name="purchase.request.import.products"
-    )
-    product_id = fields.Many2one(
-        string="Product", comodel_name="product.product", required=True
-    )
-    quantity = fields.Float(
-        digits="Product Unit of Measure", default=1.0, required=True
-    )
-    date_required = fields.Date(
-        string="Request Date",
-        required=True,
-        default=fields.Date.context_today,
-    )
-    estimated_cost = fields.Monetary(
-        string="Estimated Cost",
-        currency_field="currency_id",
-        default=0.0,
-        help="Estimated cost of Purchase Request Line, not propagated to PO.",
-    )
-    currency_id = fields.Many2one(
-        related="wizard_id.request_id.company_id.currency_id", readonly=True
-    )
