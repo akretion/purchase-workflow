469faa04e65ef960a1294354163f9ae7916e51d5
Raphaël Valyi <rvalyi@akretion.com>
2025-07-04 15:50:21 +0000

    WIP

--- purchase_bill_matching/tests/test_bill_matching.py
+++ purchase_bill_matching/tests/test_bill_matching.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from unittest.mock import patch
 from odoo import fields
 from odoo.tests import common, tagged
 from odoo.exceptions import UserError
@@ -6,14 +7,13 @@ from odoo.exceptions import UserError
 @tagged('post_install', '-at_install')
 class TestBillMatching(common.TransactionCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.partner_a = cls.env['res.partner'].create({'name': 'Test Vendor Partner'})
-        uom_unit = cls.env.ref('uom.product_uom_unit')
+    def setUp(self):
+        super().setUp()
+        self.env = self.env(context=dict(self.env.context, tracking_disable=True))
+        self.partner_a = self.env['res.partner'].create({'name': 'Test Vendor Partner'})
+        uom_unit = self.env.ref('uom.product_uom_unit')
 
-        cls.product_order = cls.env['product.product'].create({
+        self.product_order = self.env['product.product'].create({
             'name': "Test Product Ordered",
             'standard_price': 235.0,
             'list_price': 280.0,
@@ -23,7 +23,7 @@ class TestBillMatching(common.TransactionCase):
             'purchase_method': 'purchase',
             'taxes_id': False,
         })
-        cls.product_order_var_name = cls.env['product.product'].create({
+        self.product_order_var_name = self.env['product.product'].create({
             'name': "Test Product Ordered Var Name",
             'standard_price': 235.0,
             'list_price': 280.0,
@@ -31,6 +31,14 @@ class TestBillMatching(common.TransactionCase):
             'uom_id': uom_unit.id,
             'uom_po_id': uom_unit.id,
             'purchase_method': 'purchase',
+            'taxes_id': False,
+        })
+        # Create the down payment product here to be available for the wizard
+        self.dp_product = self.env['product.product'].create({
+            'name': 'Down Payment',
+            'type': 'service',
+            'purchase_ok': True,
+            'sale_ok': False,
             'taxes_id': False,
         })
 
@@ -60,10 +68,11 @@ class TestBillMatching(common.TransactionCase):
             'invoice_date': fields.Date.today(),
             'invoice_line_ids': [
                 (0, 0, {
-                    'product_id': product.id,
-                    'price_unit': product.list_price,
+                    'product_id': product.id if product else False,
+                    'name': product.name if product else 'Down Payment',
+                    'price_unit': product.list_price if product else 69.00,
                     'quantity': 1,
-                }) for product in products or []
+                }) for product in products or [False]
             ]
         }
         bill = self.env['account.move'].create(bill_vals)
@@ -92,34 +101,37 @@ class TestBillMatching(common.TransactionCase):
     def test_add_bill_to_po_downpayment(self):
         po = self.init_purchase(confirm=True, products=[self.product_order])
 
-        dp_bill_vals = {
-            'partner_id': self.partner_a.id,
-            'move_type': 'in_invoice',
-            'invoice_date': fields.Date.today(),
-            'invoice_line_ids': [(0, 0, {'name': 'Down payment', 'quantity': 1, 'price_unit': 69.00})]
-        }
-        dp_bill = self.env['account.move'].create(dp_bill_vals)
-        dp_bill.action_post()
+        dp_bill = self.init_bill(products=[], post=True)
         self.env.flush_all()
 
         match_lines = self.env['purchase.bill.line.match'].search([('aml_id', '=', dp_bill.invoice_line_ids.id)])
         action = match_lines.action_add_to_po()
         context = dict(action['context'], active_ids=match_lines.ids)
 
-        wizard = self.env['bill.to.po.wizard'].with_context(context).create({'purchase_order_id': po.id})
-        wizard.action_add_downpayment()
+        # Use unittest.mock.patch to safely mock the method
+        with patch('odoo.addons.purchase_bill_matching.wizard.bill_to_po_wizard.BillToPoWizard._get_downpayment_product', return_value=self.dp_product):
+            wizard = self.env['bill.to.po.wizard'].with_context(context).create({'purchase_order_id': po.id})
+            wizard.action_add_downpayment()
 
-        po_dp_section_line = po.order_line.filtered(lambda l: l.display_type == 'line_section' and l.is_downpayment)
-        self.assertEqual(len(po_dp_section_line), 1, "A down payment section should be created.")
-        po_dp_line = po.order_line.filtered(lambda l: not l.display_type and l.is_downpayment)
-        self.assertTrue(po_dp_line.is_downpayment)
-        self.assertEqual(po_dp_line.price_unit, -69.00) # Asserting the correct negative value
-        self.assertEqual(po_dp_line.product_qty, 1) # Quantity should be positive
+        po_dp_line = po.order_line.filtered(lambda l: l.is_downpayment and l.display_type is False)
+        self.assertTrue(po_dp_line, "Down payment line should be created on the PO.")
+        self.assertEqual(len(po_dp_line), 1, "There should be only one accountable down payment line.")
+        self.assertEqual(po_dp_line.price_unit, -69.00)
+        self.assertEqual(po_dp_line.product_qty, 1)
 
         po.order_line.filtered(lambda l: not l.is_downpayment)[0].qty_received = 1
         action_view_bill = po.action_create_invoice()
         generated_bill = self.env['account.move'].browse(action_view_bill['res_id'])
 
-        self.assertEqual(len(generated_bill.invoice_line_ids), 2) # Product line and down payment line
-        self.assertIn(-69.00, generated_bill.invoice_line_ids.mapped('price_unit'))
+        self.assertEqual(len(generated_bill.invoice_line_ids), 3, "Final bill should have 3 lines (product, dp_section, dp_line).")
+
+        product_line = generated_bill.invoice_line_ids.filtered(lambda l: l.product_id == self.product_order)
+        downpayment_line = generated_bill.invoice_line_ids.filtered(
+            lambda l: l.purchase_line_id.is_downpayment and l.display_type == 'product'
+        )
+
+        self.assertEqual(len(downpayment_line), 1, "There should be only one down payment deduction line on the bill.")
+        self.assertAlmostEqual(product_line.price_subtotal, self.product_order.list_price)
+        self.assertAlmostEqual(downpayment_line.price_subtotal, -69.00)
+
         self.assertAlmostEqual(generated_bill.amount_total, self.product_order.list_price - 69.00)
